f48579dae63146e0c18fb348d78249f840c19c1f
derek <chenkai.j@foxmail.com>
2017-08-09 23:47:58 +0800

    /learn second commit, learn/cs is ok, but learn/ is not ok due to a problem concerned about url

--- judge/admin.py
+++ judge/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import Course, Lesson, ToLearn
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(ToLearn)
--- judge/views.py
+++ judge/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from . models import Course, Lesson, ToLearn
 
 def learn(request):
-    return render(request, 'learn/learn.html')
+    all_courses = Course.objects.all()
+    return render(request, 'learn/learn.html', {'all_courses': all_courses})
+
+def course_detail(request, pk):
+    course = Course.objects.get(course_name=pk) #get_object_or_404(Course)
+    all_lessons = Lesson.objects.filter(course=course)
+    proportion = ToLearn.objects.get(course=course).lesson_num / course.total_lesson
+    return render(request, 'learn/course_detail.html',{'course': course, "all_lessons": all_lessons, "proportion": proportion})
 
 @login_required
 def course1(request):
