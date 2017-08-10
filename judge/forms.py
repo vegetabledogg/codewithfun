dc5a163790b96b1dec49d01c22b854201cd922dd
YuNanlong <yunanlong@126.com>
2017-08-10 10:24:49 +0800

    lesson页面部分提交 仅仅设计了表单，以及完成部分views逻辑

---
+++ judge/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from judge.models import Lesson, Submission
+
+class SubmissionForm(forms.ModelForm):
+    code = forms.CharField(widget=forms.Textarea)
+        
+    class Meta:
+        model = Submission
+        field = ['code']
--- judge/views.py
+++ judge/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from judge.forms import SubmissionForm
+from accounts.models import Profile
+from judge.models import Lesson, Submission
+from judge.task import evaluate_submission
 
 def learn(request):
     return render(request, 'learn/learn.html')
@@ -7,3 +11,19 @@ def learn(request):
 @login_required
 def course1(request):
     return render(request, 'learn/course_base.html')
+
+@login_required
+def lesson(request, lid):
+    if request.method == 'POST':
+        form = SubmissionForm(request.POST)
+        if form.is_valid():
+            submission = form.save()
+            submission.lesson = Lesson.objects.get(pk=lid)
+            submission.submitter = Profile.objects.get(user=request.user)
+            submission.save()
+            evaluate_submission.delay(submission.id)
+            return render()
+    else:
+        form = SubmissionForm()
+        return render()
+
