9d33ad38606fa476defbc717ab79fbd79a82908b
derek <chenkai.j@foxmail.com>
2017-08-11 12:21:40 +0800

    修改settings

--- codewithfun/settings.py
+++ codewithfun/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'codewithfun.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'codewithfun',
+        'USER': 'codewithfun',
+        'PASSWORD': 'codewithfun'
+        'PORT': '3306',
     }
 }
 
