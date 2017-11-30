38a46883bb39b5083705e2d4ec79eced3484c5d3
YuNanlong <yunanlong@126.com>
2017-11-30 23:53:20 +0800

    前后端整合v0.1

---
+++ static/js/formattohtml.js
@@ -0,0 +1,15 @@
+function HTMLEncode(html) {
+    var temp = document.createElement("div");
+    (temp.textContent != null) ? (temp.textContent = html) : (temp.innerText = html);
+    var output = temp.innerHTML;
+    temp = null;
+    return output;
+}
+
+function HTMLDecode(text) {
+    var temp = document.createElement("div");
+    temp.innerHTML = text;
+    var output = temp.innerText || temp.textContent;
+    temp = null;
+    return output;
+}
